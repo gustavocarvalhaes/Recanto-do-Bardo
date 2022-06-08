dd37d16e9daea30075580c7532c2bb72f0a2bbd4
gabriellac2002 <gabriellacsilva2002@gmail.com>
2022-06-08 17:08:15 -0300

    incluir nome dos integrantes nos arquivos

--- src/main/java/com/mycompany/recantodobardo/models/Mesa.java
+++ src/main/java/com/mycompany/recantodobardo/models/Mesa.java
@@ -4,10 +4,10 @@
  */
 package com.mycompany.recantodobardo.models;
 
-/**
- *
- * @author Ana Beatriz
- */
+ //   Ana Beatriz Lana Maciel Moreira Armond (SilverBlitzy) 202165501B
+  //  Gabriella Cruz e Silva (gabriellac2002) 202165512B
+  //  Gustavo Carvalhaes da Fonseca Ferreira (gustavirus) 202065522B
+   // Pedro Souza Pinheiro da Silva Araujo (pedro123063) 202165560C 
 public class Mesa {
     
     private Integer idMesa;
--- src/main/java/com/mycompany/recantodobardo/models/Reserva.java
+++ src/main/java/com/mycompany/recantodobardo/models/Reserva.java
@@ -5,10 +5,10 @@
 package com.mycompany.recantodobardo.models;
 import java.util.ArrayList;
 
-/**
- *
- * @author Ana Beatriz
- */
+ //   Ana Beatriz Lana Maciel Moreira Armond (SilverBlitzy) 202165501B
+  //  Gabriella Cruz e Silva (gabriellac2002) 202165512B
+  //  Gustavo Carvalhaes da Fonseca Ferreira (gustavirus) 202065522B
+   // Pedro Souza Pinheiro da Silva Araujo (pedro123063) 202165560C 
 public class Reserva {
     
     private Integer idReserva;
