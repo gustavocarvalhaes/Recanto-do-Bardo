ab19d8c23d0ad946303ea710efb1328f708845c2
gabriellac2002 <gabriellacsilva2002@gmail.com>
2022-07-11 19:34:14 -0300

    tela login ok

--- src/main/java/com/mycompany/recantodobardo/view/TelaLogin.java
+++ src/main/java/com/mycompany/recantodobardo/view/TelaLogin.java
@@ -5,6 +5,7 @@
 package com.mycompany.recantodobardo.view;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import javax.swing.*;
 /**
@@ -29,19 +30,20 @@ public class TelaLogin extends JFrame{
         this.setSize(640,480);
         this.setVisible(true);
         
-        GridLayout grid = new GridLayout(3, 2);
-        grid.setVgap(40);
-        grid.setHgap(30);
+        //GridLayout grid = new GridLayout(3, 2);
+        //grid.setVgap(30);
+        //grid.setHgap();
         
         this.formLogin = new JPanel();
-        this.formLogin.setLayout(grid);
+        this.formLogin.setLayout(new FlowLayout());
+        this.formLogin.setBorder(BorderFactory.createTitledBorder("Login"));
         this.add(formLogin);
         
         //panel auxiliar
         JPanel espaco1 = new JPanel();
         espaco1.setSize(300,200);
-        espaco1.setBorder(BorderFactory.createTitledBorder("Login"));
-        espaco1.setLayout(new GridLayout(4,2));
+        
+        espaco1.setLayout(new GridLayout(4,1));
         JPanel espaco2 = new JPanel();
         espaco2.setSize(300,200);
         espaco2.setLayout(new GridLayout(4,1));
@@ -53,20 +55,19 @@ public class TelaLogin extends JFrame{
         //email
         JLabel label_email = new JLabel("Email:");
         espaco1.add(label_email);
-        this.text_field_email = new JTextField();
-        this.text_field_email.setPreferredSize(new Dimension(100,10));
-        espaco1.add(text_field_email);
+        this.text_field_email = new JTextField(50);
+        //this.text_field_email.setPreferredSize(new Dimension(100,10));
+        espaco2.add(text_field_email);
         
         //senha
-        //JLabel label_senha = new JLabel("Senha:");
-        //formLogin.add(label_senha);
-        //this.text_field_senha = new JTextField();
-        //formLogin.add(text_field_senha);
+        JLabel label_senha = new JLabel("Senha:");
+        espaco1.add(label_senha);
+        this.text_field_senha = new JTextField(50);
+        espaco2.add(text_field_senha);
         
         //button
-        //this.button_login = new JButton("Ok");
-        //formLogin.add(button_login);
-       //formLogin.add(this.button_login);
+        this.button_login = new JButton("Ok");
+        espaco1.add(this.button_login);
        
     }
     
