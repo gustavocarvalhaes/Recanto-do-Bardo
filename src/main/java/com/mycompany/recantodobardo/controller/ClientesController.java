f7d3cd30e022b4d9598108dec04939744b73bad8
cidadcruz cida <gabriellacsilva2002@gmail.com>
2022-07-15 16:38:43 -0300

    tela de cadastro clientes ok

---
+++ src/main/java/com/mycompany/recantodobardo/controller/ClientesController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.recantodobardo.controller;
+import com.mycompany.recantodobardo.view.CadastroClientes;
+/**
+ *
+ * @author cdcru
+ */
+public class ClientesController {
+    public static void main(String[] args) {
+        CadastroClientes tela_cadastro = new CadastroClientes();
+        tela_cadastro.setVisible(true);
+    }
+}
