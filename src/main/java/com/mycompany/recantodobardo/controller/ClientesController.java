1c1dc1ede418bc8e956669608a29955e2002b27f
cidadcruz cida <gabriellacsilva2002@gmail.com>
2022-07-18 15:25:34 -0300

    menu Clientes ok

--- src/main/java/com/mycompany/recantodobardo/controller/ClientesController.java
+++ src/main/java/com/mycompany/recantodobardo/controller/ClientesController.java
@@ -4,13 +4,17 @@
  */
 package com.mycompany.recantodobardo.controller;
 import com.mycompany.recantodobardo.view.CadastroClientes;
+import com.mycompany.recantodobardo.view.MenuClientes;
 /**
  *
  * @author cdcru
  */
 public class ClientesController {
     public static void main(String[] args) {
-        CadastroClientes tela_cadastro = new CadastroClientes();
-        tela_cadastro.setVisible(true);
+        //CadastroClientes tela_cadastro = new CadastroClientes();
+       // tela_cadastro.setVisible(true);
+       
+       MenuClientes menu = new MenuClientes();
+       menu.setVisible(true);
     }
 }
---
+++ src/main/java/com/mycompany/recantodobardo/view/MenuClientes.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.recantodobardo.view;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author cdcru
+ */
+public class MenuClientes extends JFrame{
+    
+    private JButton button_lista;
+    private JButton button_cadastro;
+    private JButton button_voltar;
+    private JPanel menuClientes;
+    
+   public MenuClientes(){
+       inicializar();
+   }
+    
+    private void inicializar(){
+        this.setTitle("Login");
+        this.setSize(640,480);
+        this.setVisible(true);
+        
+        this.setMenuClientes(new JPanel());
+        this.getMenuClientes().setLayout(new FlowLayout());
+        this.getMenuClientes().setBorder(BorderFactory.createTitledBorder("Menu Clientes"));
+        this.add(getMenuClientes());
+        
+        //panel auxiliar
+        JPanel espaco1 = new JPanel();
+        espaco1.setSize(300,200);
+        espaco1.setLayout(new GridLayout(4,1));
+        
+        getMenuClientes().add(espaco1);
+
+        JLabel label_titulo = new JLabel("---- Clientes ----");
+        espaco1.add(label_titulo);
+        this.setButton_voltar(new JButton("Voltar"));
+        espaco1.add(this.button_voltar);
+       
+        this.setButton_cadastro(new JButton("Cadastrar"));
+        espaco1.add(this.button_cadastro);
+        
+        this.setButton_lista(new JButton("Listar"));
+        espaco1.add(this.button_lista);
+    }
+
+  
+    public JButton getButton_lista() {
+        return button_lista;
+    }
+
+
+    public void setButton_lista(JButton button_lista) {
+        this.button_lista = button_lista;
+    }
+
+
+    public JButton getButton_cadastro() {
+        return button_cadastro;
+    }
+
+
+    public void setButton_cadastro(JButton button_cadastro) {
+        this.button_cadastro = button_cadastro;
+    }
+
+    public JButton getButton_voltar() {
+        return button_voltar;
+    }
+
+
+    public void setButton_voltar(JButton button_voltar) {
+        this.button_voltar = button_voltar;
+    }
+
+    public JPanel getMenuClientes() {
+        return menuClientes;
+    }
+
+
+    public void setMenuClientes(JPanel menuClientes) {
+        this.menuClientes = menuClientes;
+    }
+}
